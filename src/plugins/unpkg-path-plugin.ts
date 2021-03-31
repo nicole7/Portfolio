b7cf04fe17bf12d4c3f6e8289168f2895a58d5e7
Nicole <ngasperini@ussilica.com>
2021-03-31 10:07:55 -0500

    change package plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -8,26 +8,24 @@ export const unpkgPathPlugin = () => {
     name: 'unpkg-path-plugin',
     //This function will be called by ESBuild - represents the bundling process
     setup(build: esbuild.PluginBuild) {
+      //resolve - find the file
+      //filter is a regular expression - to control when resolve and load are executed 
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResole', args);
+        //namespace here must be the same if applied to onLoad
         return { path: args.path, namespace: 'a' };
       });
- 
+      //load the file
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log('onLoad', args);
- 
+        //giving ESBuild the files and body directly
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              import message from './tiny-test-pkg';
               console.log(message);
             `,
-          };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
           };
         }
       });
