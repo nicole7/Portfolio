207931bb76eab57b2f374c482cee5b9017f6567f
Nicole <ngasperini@ussilica.com>
2021-04-09 08:20:12 -0500

    local api setup with express, connecting the proxy

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,5 +1,6 @@
 import { Command } from 'commander';
 import { serve } from 'local-api';
+import { PRIORITY_BELOW_NORMAL } from 'node:constants';
 import path from 'path';
 
 //square brackets indicate valie is optional and angled brackets are required
@@ -7,7 +8,20 @@ export const serveCommand = new Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'Port to run server on', '4005')
-    .action((filename = 'notebook.js', options: { port: string }) => {
-        const dir = path.join(process.cwd(), path.dirname(filename))
-        serve(parseInt(options.port), path.basename(filename), dir)
+    .action(async (filename = 'notebook.js', options: { port: string }) => {
+        try {
+            const dir = path.join(process.cwd(), path.dirname(filename))
+            await serve(parseInt(options.port), path.basename(filename), dir)
+            console.log(
+                `Opened ${filename}. Navigate to http://localhost:${options.port} to edit file`
+            )
+        } catch (err) {
+            if (err.code === 'EADDRINUSE') {
+                console.log('Port is in use. Try running on different port');
+            } else {
+                console.log('Heres the problem', err.message)
+            }
+            //force exit program with status code of 1 - unsuccessful run of prgram
+            process.exit(1);
+        }
     });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,15 @@
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 export const serve = (port: number, filename: string, dir: string) => {
-    console.log("serving traffic on port", port)
-    console.log('saving/fetching cells from', filename)
-    console.log('that file its in dir', dir)
+    const app = express();
+    app.use(createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent'
+    }))
+    //bring express into async
+    return new Promise<void>((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
